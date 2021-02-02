f45f769bbcc371d891ef6a8e2467e4ac2cdfd3c2
Silinde87 <paurodriguezmolina@gmail.com>
2021-02-02 19:26:59 +0100

    sounds working with onclick event and keyboard

--- script.js
+++ script.js
@@ -1,1 +1,92 @@
-//script
+const sounds = {
+    81:{
+        keyCode: 81,
+        keyTrigger: 'Q',
+        id: 'C4',
+        note: '/sounds/Piano.mf.C4.wav'
+    },
+    87:{
+        keyCode: 87,
+        keyTrigger: 'W',
+        id: 'D4',
+        note: '/sounds/Piano.mf.D4.wav'
+    },
+    69:{
+        keyCode: 69,
+        keyTrigger: 'E',
+        id: 'E4',
+        note: '/sounds/Piano.mf.E4.wav'
+    },
+    65:{
+        keyCode: 65,
+        keyTrigger: 'A',
+        id: 'F4',
+        note: '/sounds/Piano.mf.F4.wav'
+    },
+    83:{
+        keyCode: 83,
+        keyTrigger: 'S',
+        id: 'G4',
+        note: '/sounds/Piano.mf.G4.wav'
+    },
+    68:{
+        keyCode: 68,
+        keyTrigger: 'D',
+        id: 'A4',
+        note: '/sounds/Piano.mf.A4.wav'
+    },
+    90:{
+        keyCode: 90,
+        keyTrigger: 'Z',
+        id: "B4",
+        note: '/sounds/Piano.mf.B4.wav'
+    },
+    88:{
+        keyCode: 88,
+        keyTrigger: 'X',
+        id: 'C5',
+        note: '/sounds/Piano.mf.C5.wav'
+    },
+    67:{
+        keyCode: 67,
+        keyTrigger: 'C',
+        id: 'D5',
+        note: '/sounds/Piano.mf.D5.wav'
+    }
+};
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+    const sound = new Audio(sounds[e].note);
+    let key = document.querySelector(`#${sounds[e].keyTrigger}`);
+    key.classList.add('playing');
+    console.log(sound);
+    sound.play();
+}
+
+const btn = document.querySelectorAll('.drum-pad');
+btn.forEach((button) => {
+    button.addEventListener('click', () => {
+        Object.getOwnPropertyNames(sounds).some(key => {
+            if(sounds[key].keyTrigger === button.textContent){
+                playSound(key);
+            }
+        });
+    });
+});
+
+
+$(document).keydown(function(e){
+    Object.getOwnPropertyNames(sounds).some(key =>{
+        if(sounds[key].keyCode === e.which){
+            playSound(key);
+        }
+    });
+});
+
+const keys = Array.from(document.querySelectorAll('.drum-pad'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
